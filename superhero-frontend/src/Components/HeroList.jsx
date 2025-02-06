b35589719b38d863d391dd7651d0b1c1c0b64f87
Kemal Durukan MERT <103702848+KemalDurukanMERT@users.noreply.github.com>
2025-02-06 03:42:15 +0100

    Hero List Completed

--- superhero-frontend/src/Components/HeroList.jsx
+++ superhero-frontend/src/Components/HeroList.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const HeroList = () => {
+const HeroList = ({ heroes }) => {
   return (
-    <div>HeroList</div>
-  )
-}
+    <div className="container">
+      <ul className="superheroes d-flex justify-content-center align-items-center my-5 gap-5 flex-wrap">
+        {heroes.map((hero, index) => (
+          <li key={index} className="card p-4  bg-warning">
+            <h2 className="text-center">{hero.name}</h2>
+            <p>
+              <b>
+                <i>Superpower:</i>
+              </b>
+              {hero.superpower}
+            </p>
+            <p>
+              <b>
+                <i>Humility Score:</i>
+              </b>
+              {hero.humility_score}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default HeroList
+export default HeroList;
