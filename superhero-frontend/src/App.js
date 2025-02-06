c991a6bf68b801f3fd12d18534640b6605d4d16a
Kemal Durukan MERT <103702848+KemalDurukanMERT@users.noreply.github.com>
2025-02-06 03:37:06 +0100

    Component Structure Created

--- superhero-frontend/src/App.js
+++ superhero-frontend/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import HeroForm from "./Components/HeroForm";
+import HeroList from "./Components/HeroList";
 
 function App() {
+  const [heroes, setHeroes] = useState([]);
+  const [error, setError] = useState();
+
+  const getHeroes = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8000/api/v1/superheroes"
+      );
+      setHeroes(response.data.data.heroes);
+    } catch (error) {
+      setError(error);
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    }
+  };
+
+  useEffect(() => {
+    getHeroes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-center my-5 text-danger ">Humble Superheroes</h1>
+      <HeroForm getHeroes={getHeroes} />
+      {error && <p className="text-center text-danger fs-3 mt-2">{error}</p>}
+      <HeroList heroes={heroes} />
     </div>
   );
 }
